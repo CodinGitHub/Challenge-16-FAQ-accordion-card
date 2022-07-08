c888970dd2d85fef48127e86ebf705b63a7ea1a7
CodinGitHub <gms2programador@gmail.com>
2022-07-08 17:26:16 -0500

    responsive layout

---
+++ main.js
@@ -0,0 +1,8 @@
+let elements = document.querySelectorAll('.main-container__details__sumary');
+
+let arrayElements = [...elements]
+
+console.log(elements)
+console.log(arrayElements)
+
+elements.forEach(item => console.log(item))
